f66a0b4ab4b6ed82ecbb6238e48441cb599fe435
Ahnsj828 <ala828@naver.com>
2024-08-11 05:40:14 +0900

    change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faL, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faSearch } from "@fortawesome/free-solid-svg-icons";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
 
 const menuList = [
@@ -20,7 +20,6 @@ function Navbar({ authenticate, setAuthenticate }) {
   const onCheckEnter = (e) => {
     if (e.key === "Enter") {
       navigate(`?q=${e.target.value}`);
-      // 쿼리 스트링값으로 전달해라
       e.target.value = "";
     }
   };
--- src/components/ProductAll.js
+++ src/components/ProductAll.js
@@ -1,27 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
-// useSearchParams 상단에 쿼리값을 찾아오는 리액트 훅
 import ProductCard from "./ProductCard";
 import { Container, Row, Col } from "react-bootstrap";
 
 const ProductAll = () => {
   const [productList, setProductList] = useState([]);
   const [query, setQuery] = useSearchParams();
-  // query => state 값 / setQuery => state 변수값
-  // console.log(query); query는 객체다
-
   const searchQuery = query.get("q") || "";
-  // q=재킷 q라는 key에 해당하는 value값 재킷
-  // 단락회로평가
-  // || 또는 => 좌항이 참이 아니더라도 넘어가라  /   || "";=> 없어도 넘어가라
-  // && 좌항이 참이여야 넘어간다
-
   console.log(searchQuery);
-
   const getProducts = async () => {
-    const url = ` https://my-json-server.typicode.com/Ahnsj828/musinsashopping/products?q=${searchQuery}`;
-    // Ahnsj828 깃허브 아이디
-    // musinsashopping 깃허브 레파지토리 이름
+    const url = `https://my-json-server.typicode.com/Divjason/musinsashopping/products?q=${searchQuery}`;
     const response = await fetch(url);
     const data = await response.json();
     setProductList(data);
@@ -29,9 +17,6 @@ const ProductAll = () => {
   useEffect(() => {
     getProducts();
   }, [query]);
-  // 재킷을 검색하면 재킷에 매칭되는 상품들을 다시 리랜더링
-  // 원피스를 검색하면 원피스에 매칭되는 상품들을 다시 리랜더링
-
   return (
     <Container>
       <Row>
